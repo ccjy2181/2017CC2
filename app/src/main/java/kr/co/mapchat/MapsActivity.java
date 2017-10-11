46c8997f619aa2f99cb12c667bfcb4142d6903fa
DESKTOP-PA3FO6L\CJY <ccjy2181@naver.com>
2017-10-11 20:16:55 +0900

    MapActivity Layout 수정중 (미완)

--- app/src/main/java/kr/co/mapchat/MapsActivity.java
+++ app/src/main/java/kr/co/mapchat/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String initAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
