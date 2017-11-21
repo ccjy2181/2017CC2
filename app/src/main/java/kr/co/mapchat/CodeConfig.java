107b7adcbd1597cc0eccfe7e1d1ff6cc58dffa70
CJY <ccjy2181@naver.com>
2017-11-22 05:03:11 +0900

    갈아엎음 Ver.0.3

---
+++ app/src/main/java/kr/co/mapchat/CodeConfig.java
@@ -0,0 +1,8 @@
+package kr.co.mapchat;
+
+
+public class CodeConfig {
+    public final double MJU_LATITUDE = 37.2221794128418;
+    public final double MJU_LONGITUDE = 127.1860580444336;
+    public final String DAUM_MAP_API_KEY = "f7463307e96ec8803cdb9190cd4678ea";
+}
--- app/src/main/java/kr/co/mapchat/FragmentMap.java
+++ app/src/main/java/kr/co/mapchat/FragmentMap.java
@@ -6,20 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
+import static android.view.KeyEvent.ACTION_UP;
 
 
 /**
  * Created by Dytstudio.
  */
 
-public class FragmentMap extends Fragment {
+public class FragmentMap extends Fragment implements MapView.MapViewEventListener{
+    CodeConfig codeConfig = new CodeConfig();
     private MapView mapView;
+    RelativeLayout mapViewContainer;
+    View view;
+
+    double[] location = {0,0};
 
     public FragmentMap(){
         setHasOptionsMenu(true);
@@ -30,14 +38,13 @@ public class FragmentMap extends Fragment {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, null, false);
+        view = inflater.inflate(R.layout.fragment_map, container, false);
 
         mapView = new MapView(this.getContext());
         getActivity().supportInvalidateOptionsMenu();
         ((MainActivity)getActivity()).changeTitle(R.id.toolbar, "Map");
 
-        RelativeLayout mapViewContainer = (RelativeLayout)view.findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+        mapViewContainer = (RelativeLayout)view.findViewById(R.id.map_view);
 
         return view;
     }
@@ -45,5 +52,66 @@ public class FragmentMap extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
         inflater.inflate(R.menu.menu_edit, menu);
+    }
+
+    public void resetFragment(){
+        mapViewContainer.removeView(mapView);
+        if(mapViewContainer.indexOfChild(mapView) == -1) {
+            initMapView();
+        }
+    }
+
+    public void initMapView(){
+        mapView = new MapView(view.getContext());
+
+        mapViewContainer.addView(mapView, 0);
+    }
+
+    // MapView.MapViewEventListener
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        location[0] = location[0] == 0 ? this.codeConfig.MJU_LATITUDE : location[0];
+        location[1] = location[1] == 0 ? this.codeConfig.MJU_LONGITUDE : location[1];
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(location[0], location[1]), 3, true);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
     }
 }
