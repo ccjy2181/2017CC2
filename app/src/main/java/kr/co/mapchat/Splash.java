87a662d5f484f42241e424b34224e3be06827eb8
Nam <namjjang6@gmail.com>
2017-11-29 05:13:27 +0900

    splash -> 메인으로

--- app/src/main/java/kr/co/mapchat/Splash.java
+++ app/src/main/java/kr/co/mapchat/Splash.java
@@ -15,17 +15,83 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 import static com.kakao.util.maps.helper.Utility.getPackageInfo;
 
 public class Splash extends AppCompatActivity {
 
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+//                "010-4553-2181",        // Phone number to verify
+//                60,                 // Timeout duration
+//                TimeUnit.SECONDS,   // Unit of timeout
+//                this,               // Activity (for callback binding)
+//                mCallbacks);        // OnVerificationStateChangedCallbacks
+//
+//        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+//
+//            @Override
+//            public void onVerificationCompleted(PhoneAuthCredential credential) {
+//                // This callback will be invoked in two situations:
+//                // 1 - Instant verification. In some cases the phone number can be instantly
+//                //     verified without needing to send or enter a verification code.
+//                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+//                //     detect the incoming verification SMS and perform verificaiton without
+//                //     user action.
+//                System.out.println("onVerificationCompleted:" + credential);
+//
+//               // signInWithPhoneAuthCredential(credential);
+//            }
+//
+//            @Override
+//            public void onVerificationFailed(FirebaseException e) {
+//                // This callback is invoked in an invalid request for verification is made,
+//                // for instance if the the phone number format is not valid.
+//                System.out.println("onVerificationFailed"+e);
+//
+//                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+//                    // Invalid request
+//                    // ...
+//                } else if (e instanceof FirebaseTooManyRequestsException) {
+//                    // The SMS quota for the project has been exceeded
+//                    // ...
+//                }
+//
+//                // Show a message and update the UI
+//                // ...
+//            }
+//
+//            @Override
+//            public void onCodeSent(String verificationId,
+//                                   PhoneAuthProvider.ForceResendingToken token) {
+//                // The SMS verification code has been sent to the provided phone number, we
+//                // now need to ask the user to enter the code and then construct a credential
+//                // by combining the code with a verification ID.
+//                System.out.println("onCodeSent:" + verificationId);
+//
+//                // Save verification ID and resending token so we can use them later
+//               // mVerificationId = verificationId;
+//               // mResendToken = token;
+//
+//                // ...
+//            }
+//        };
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -35,7 +101,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Splash.this, SetupPhoneNumber.class));
+                startActivity(new Intent(Splash.this, MainActivity.class));
                 finish();
             }
         }, 5*1000);
